1e57558fd6878c3b4e162207607732842eddfc6c
Ratnasambhav Priyadarshi <ratnasambhav@gmail.com>
2016-11-30 13:09:01 +0530

    Added app.js
    The main javascript file.

---
+++ app.js
@@ -0,0 +1,20 @@
+function getColor(){
+  return '#' + Math.random().toString(16).slice(2,8);
+}
+
+function setColor(color){
+  document.body.style.background = color;
+}
+
+function setAndPrintColor(){
+  var color = getColor();
+  setColor(color);
+  document.getElementById("hex").innerHTML = color;
+}
+document.body.onkeyup = function(e){
+  if(e.keyCode == 32){
+    setAndPrintColor();
+  }
+};
+
+document.getElementById("change").addEventListener("click", setAndPrintColor);
